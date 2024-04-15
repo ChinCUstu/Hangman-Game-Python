6254ce102fb593dad7904afc5a3334b0c55a6b9e
ChinCUstu <ndulue.chinedu@stu.cu.edu.ng>
2024-04-15 02:57:24 +0100

    Enhanced user experience by importing word list from hangman_words.py file and integrating hangman art for improved visual appeal. Logo now displayed at the start of the game.

--- Day 7/Hangman.py
+++ Day 7/Hangman.py
@@ -1,87 +1,31 @@
 import random as ran
 
-stages = ['''
-  +---+
-  |   |
-  0   |
- /|\  |
- / \  |
-      |
-==========
-''', '''
-  +---+
-  |   |
-  0   |
- /|\  |
- /    |
-      |
-==========
-''', '''
-  +---+
-  |   |
-  0   |
- /|\  |
-      |
-      |
-==========
-''', '''
-  +---+
-  |   |
-  0   |
- /|   |
-      |
-      |
-==========
+import hangman_art as art
+import hangman_words as words
 
-''', '''
-  +---+
-  |   |
-  0   |
- /    |
-      |
-      |
-==========
+print(art.logo)
 
-''', '''
-  +---+
-  |   |
-  0   |
-      |
-      |
-      |
-==========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-==========
-''']
+chosen_word = ran.choice(words.word_list)
+count = 0
 
-# Picking a random words and checking Answers
-word_list = ["aardvark", "baboon", "camel"]
 lives = 6
-chosen_word = ran.choice(word_list)
+end_of_game = False
 
 print(f"Pssst, the random word is {chosen_word}")
 display = ["_"] * len(chosen_word)
 print(''.join(display))
-count = 0
-end_of_game = False
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    
-    for position in range(len(chosen_word)):
-        if guess == chosen_word[position]:
-            display[position] = chosen_word[position]
+    if guess in chosen_word:
+        for position in range(len(chosen_word)):
+            if guess == chosen_word[position]:
+                display[position] = chosen_word[position]
     else:
         lives -= 1
-    print(stages[lives])
-    if lives == 0:
-        end_of_game = True
-        print("You lose")
+        print(art.stages[lives])
+        if lives == 0:
+            end_of_game = True
+            print("You lose")
     print("".join(display))
     if "_" not in display:
         end_of_game = True
