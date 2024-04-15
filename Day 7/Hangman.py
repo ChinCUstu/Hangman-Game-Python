68b74722d52aead419f5c5fab460d4d211dca157
ChinCUstu <ndulue.chinedu@stu.cu.edu.ng>
2024-04-15 01:44:51 +0100

    Implemented game loop to allow continuous guessing. Added condition to terminate loop when word is fully guessed.

--- Day 7/Hangman.py
+++ Day 7/Hangman.py
@@ -8,10 +8,16 @@ chosen_word = ran.choice(word_list)
 print(f"Pssst, the random word is {chosen_word}")
 display = ["_"] * len(chosen_word)
 print(''.join(display))
-guess = input("Guess a letter: ").lower()
-for i in range(len(chosen_word)):
-    if guess == chosen_word[i]:
-        display[i] = chosen_word[i]
-    # else:
-    #     print("Nope")
-print(''.join(display))
+count = 0
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    
+    for position in range(len(chosen_word)):
+        if guess == chosen_word[position]:
+            display[position] = chosen_word[position]
+    print("".join(display))
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
