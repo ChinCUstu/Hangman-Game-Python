1ccefdadbe34393b37b0e7017cdbb96f95c31848
ChinCUstu <ndulue.chinedu@stu.cu.edu.ng>
2024-04-16 02:30:27 +0100

    updated

--- Day 8/Caesar-Cipher.py
+++ Day 8/Caesar-Cipher.py
@@ -1,30 +1,32 @@
 import art as a
+
 print(a.logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
 def caesar(start_text, shift_amount, cipher_direction):
-    end_text = []
-    for letter in start_text:
-        position = alphabet.index(letter)
-        if cipher_direction == 'encode':
-            new_position = position + shift_amount
-            if new_position > len(alphabet):
-                newer_position = new_position - len(alphabet)
-                end_text += alphabet[newer_position]
-            else:
-                new_letter = alphabet[new_position]
-                end_text += new_letter
-        elif cipher_direction == 'decode':
-            new_position = position - shift_amount
-            new_letter = alphabet[new_position]
-            end_text += new_letter
-    return f"The {cipher_direction}d is {''.join(end_text)}"
+    end_text = ""
+    if cipher_direction == 'decode':
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = (position + shift_amount) % len(alphabet)
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"The {cipher_direction}d text is {end_text}")
 
+def start():
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % len(alphabet)
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-print(caesar(start_text=text, shift_amount=shift, cipher_direction=direction))
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == 'yes':
+        start()
+
+start()
